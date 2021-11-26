58508ef7245f2cb66d42dc346b2abe3b61645ba1
sduess <s.dussler20@imperial.ac.uk>
2021-11-26 09:14:09 +0000

    add: Flex op path file

---
+++ bin/FLEXOP_vars.sh
@@ -0,0 +1,13 @@
+# This script loads the required paths for FLEXOP
+
+if test -n "$ZSH_VERSION"; then
+  SCRIPTPATH=${0:a:h}
+elif test -n "$BASH_VERSION"; then
+  SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+fi
+
+export PATH=$SCRIPTPATH:$PATH
+echo "HALE added to PATH from the directory: "$SCRIPTPATH
+
+SCRIPTPATH=$SCRIPTPATH"/.."
+export PYTHONPATH=$SCRIPTPATH:$PYTHONPATH
